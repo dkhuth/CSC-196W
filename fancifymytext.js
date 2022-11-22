4940ad35a8402c1c4f4f80dc33f57d99e08c7245
dkhuth <dkhuth291@gmail.com>
2022-11-21 21:20:29 -0800

    assignment 4 start

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,1 +1,22 @@
-alert("Hello, world!");
+function bigger() {
+    document.getElementById("input").style.fontSize = "24pt";
+}
+
+function fancyShmancy() {
+    document.getElementById("input").style.fontWeight = "bold";
+    document.getElementById("input").style.color = 'blue';
+    document.getElementById("input").style.textDecoration = "underline";
+
+}
+
+function boringBetty() {
+    document.getElementById("input").style.fontWeight = "normal";
+    document.getElementById("input").style.color = "black";
+    document.getElementById("input").style.textDecoration = "none";
+}
+
+function moo() {
+    var str = document.getElementById("input");
+    var parts = str.value.split(".");  
+    str.value = parts.join("-Moo");
+}
