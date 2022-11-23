0f8d98fa10cf7b925e596b783dacb1f1b11bd3e1
dkhuth <dkhuth291@gmail.com>
2022-11-22 19:13:51 -0800

    working on button

--- encrypt-it.js
+++ encrypt-it.js
@@ -16,12 +16,43 @@ console.log("Window loaded!");
    * TODO: Write a function comment using JSDoc.
    */
   function init() {
-    // Note: In this function, we usually want to set up our event handlers
-    // for UI elements on the page.
+    var text = document.getElementById("input-text");
+    var shiftOrRan = document.getElementById("cipher-type").value;
+    const encrypt = document.getElementById("encrypt-it")
+    encrypt.addEventListener("click",handleClick(shiftOrRan,text));
+  }
+
+  function handleClick(value,text){
+    
+    if(value == "shift"){
+      return shiftCipher(text);
+    }
+
+
   }
 
   // Add any other functions in this area (you should not implement your
   // entire program in the init function, for similar reasons that
   // you shouldn't write an entire Java program in the main method).
-
+  /**
+ * Returns an encrypted version of the given text, where
+ * each letter is shifted alphabetically ahead by 1 letter,
+ * and 'z' is shifted to 'a' (creating an alphabetical cycle).
+ */
+function shiftCipher(text) {
+  text = text.toLowerCase();
+  let result = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] < 'a' || text[i] > 'z') {
+      result += text[i];
+    } else if (text[i] == 'z') {
+      result += 'a';
+    } else { // letter is between 'a' and 'y'
+      let letter = text.charCodeAt(i);
+      let resultLetter = String.fromCharCode(letter + 1);
+      result += resultLetter;
+    }
+  }
+  return result;
+}
 })();
